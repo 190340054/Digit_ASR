173e8eeafe07453d607933d08d55624b4879f358
190340054 <190340054@klh.edu.in>
2023-03-18 11:33:19 +0530

    downgraded to python 3.8 version and included packages librosa, rich, soundfile

--- code/cleaning_script.py
+++ code/cleaning_script.py
@@ -3,4 +3,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.environ['train_path'])
+#sample testing for correct path
+# print(os.environ['train_path'])
+
+def check_folder():
+    """
+    this is documenation space 
+    """
+    #check does we have cleaning dir
+    if os.path.exits(os.environ[clean_path]):
+        #we will do something
+        pass
+    else:
+        os.mkdir(os.environ[clean_path])
+
+def read_files():
+    """
+    this is documenation space
+    """
