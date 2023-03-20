07213f6764a622709f2085f0455abfc36ac8a7f9
fwerg5ju75 <125389856+fwerg5ju75@users.noreply.github.com>
2023-03-20 11:27:17 +0530

    resample, labeling

--- code/main.py
+++ code/main.py
@@ -2,6 +2,8 @@ import os
 import librosa
 import soundfile as sf
 from rich.progress import track
+from phonemizer.backend import EspeakBackend
+import eng_to_ipa as p
 
 path = 'C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train'
 
@@ -38,46 +40,107 @@ def get_total_M_speakers():
 # get_total_M_speakers()
 
 def format(string):
-    if string.isupper():
+    var = string.split('.')[0]
+    ext = string.split('.')[1]
+    if var.isupper():
         ans = string
     else:
-        ans = string.upper()
+        ans = var.upper() + '.' + ext
     return ans
 
 def resample():
 
     path = 'C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train/'
-    os.mkdir('C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train_16k')
     a = 'C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train_16k'
-
-    flag = 0
-    count = 0
+    os.mkdir(f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train_16k")
+    # flag = 0
+    # count = 0
     for i in track(os.listdir(path)):
-        print("inside first loop")
-        if flag == 1:
-            break
-        flag+=1
+        # print("inside first loop")
+        # if flag == 1:
+            # break
+        # flag+=1
+        os.mkdir(f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train_16k/{i}")
         for j in os.listdir(f"{path}/{i}/"):
-            print("inside secound loop")
-            if count == 1:
-                break
-            count+=1
-            audio_file = f"{path}/{i}/{j}"
-            print("extracted aduio file")
-            samples, sample_rate = librosa.load(audio_file)
-            print("loaded audio file")
+            # print("inside secound loop")
+            # if count == 1:
+                # break
+            # count+=1
+            file_name = os.path.basename(f"{path}/{i}/{j}")
+            sample_name = format(file_name)
+            destination_path = f"{a}/{i}/{sample_name}"
+            samples, sample_rate = librosa.load(f"{path}/{i}/{j}", mono = True)
             samples = librosa.resample(samples, sample_rate, 16000)
-            print("resampleing done")
-            file_name = format(f"{path}/{i}/{j}".split()[0])
-            print(f"{path}/{i}/{j}"[0])
-            print(f"{path}/{i}/{j}")
-            print("extracted file_name ----->", file_name)
-            ext = f"{path}/{i}/{j}"[1]
-            print("extracted file name extention ---->", ext)
-            file_name = file_name + ext
-            print("concatinatced ---->", file_name)
-            destination_path = f"{a}/{i}/{file_name}"
             f = open(destination_path, "x")
             sf.write(destination_path, samples, 16000)
 
-resample()
+# resample()
+
+def get_last_digit(s):
+    ext = s.split('.')[0]
+    ext = ext + '.txt'
+    d = s.split('.')[0][-1]
+    if d == ' ':
+        d = s.split('.')[0][-2]
+    return d, ext
+
+
+def labeling():
+    hash = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    path = 'C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train/'
+    os.mkdir(f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/label")
+    # flag = 0
+    for i in os.listdir(path):
+        # print("inside first loop")
+        # if flag == 1:
+        #     break
+        # flag+=1
+        # count = 0
+        os.mkdir(f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/label/{i}")
+        a = f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/label"
+        for j in os.listdir(f"{path}{i}/"):
+            # print("inside secound loop")
+            # if count == 1:
+            #     break
+            # count+=1
+            file_name = os.path.basename(f"{path}{i}/{j}")
+            digit, txt_name = get_last_digit(file_name)
+            digit = int(digit)
+            l_destination_path = f"{a}/{i}/{txt_name}"
+            for val in range(len(hash)):
+                if val == digit:
+                    ans = hash[val]
+                    break
+            f = open(l_destination_path, 'x')
+            f.write(ans)
+
+labeling()
+
+# def get_file_name(string):
+#     return string.split('.')[0]
+
+
+# def phonemes():
+#     path = 'C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/train/'
+#     os.mkdir(f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/phonemes")
+#     flag = 0
+#     for i in os.listdir(path):
+#         if flag == 1:
+#             break
+#         flag += 1
+#     count = 0
+#     os.mkdir(f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/phonemes/{i}")
+#     a = f"C:/Users/siris/OneDrive/Desktop/Documents/GitHub/Digit_ASR/Source/KLEF_Digit_Data/KLEF_Digit_Data/phonemes"
+#     for j in os.listdir(f"{path}{i}/"):
+#         if count == 1:
+#             break
+#         count += 1
+#         file_name = os.path.basename(f"{path}{i}/{j}")
+#         phone_name = get_file_name(file_name)
+#         ans = labeling()
+#         phonemized = p.convert(ans)
+#         p_destination_path = f"{a}/{i}/{phone_name}"
+#         f = open(p_destination_path, 'x')
+#         f.write(phonemized)
+#
+# phonemes()
